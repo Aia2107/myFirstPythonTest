f1be0c307afa5cc6ca20bdfc122d59f19f3e7568
Aia2107 <2107usa@gmail.com>
2025-06-26 20:00:00 -0400

    m

--- conditional.py
+++ conditional.py
@@ -24,3 +24,43 @@ if age >= 18 and is_student:
     print("Eligible for student discount")
 else:
     print("Not eligible for student discount")
+
+#commin pitfalls & tips
+
+
+# Pitfall: all conditions are checked 
+if x > 0: 
+		print("Positive") 
+if x == 0: 
+		print("Zero") 
+else: 
+		print("Negative") 
+
+# Correct approach using elif 
+if x > 0: 
+		print("Positive") 
+elif x == 0: 
+		print("Zero") 
+else: 
+		print("Negative")
+  
+bootcamp = 'coding temple'
+
+# Incorrect Way:
+#if (bootcamp = 'coding temple'): # would throw an error
+		#print('best bootcamp ever!')
+
+# Correct Way:
+if (bootcamp == 'coding temple'):
+		print('best bootcamp ever!')
+  
+#final challenge: create a program where the user has to guess a number between 1 and 10.
+import random
+secret_number = random.randint(1, 10)
+guess = int(input("Guess a number between 1 and 10: "))
+if guess == secret_number:
+    print("Congratulations! You guessed the number.")
+elif guess < secret_number:
+    print("Too low! Try again.")
+else:
+    print("Too high! Try again.")   
